579335c23ba2759edf14316deab64692e1907bc1
EdwardAndress <eddie_andress@hotmail.com>
2018-12-05 00:43:44 +0000

    Logs up/down/left/right key presses

--- src/index.js
+++ src/index.js
@@ -3,9 +3,9 @@ import 'phaser';
 import { SimpleScene } from './scenes/simple-scene';
 
 const gameConfig = {
-  width: 680,
-  height: 400,
-  scene: SimpleScene
+  width: 1000,
+  height: 500,
+  scene: SimpleScene,
 };
 
 new Phaser.Game(gameConfig);
--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -4,7 +4,35 @@ export class SimpleScene extends Phaser.Scene {
   }
 
   create() {
+    console.log(this)
     this.add.text(100, 100, 'Hello Phaser!', { fill: '#0f0' });
-    this.add.image(100, 200, 'cokecan');
+    this.player = this.add.sprite(200, 400, 'cokecan');
+    this.cursors = this.input.keyboard.createCursorKeys();
+  }
+
+  update() {
+
+    if (this.cursors.up.isDown)
+    {
+        console.log('up');
+        this.player.body.moveUp(300)
+    }
+    else if (this.cursors.down.isDown)
+    {
+        console.log('right');
+        this.player.body.moveDown(300);
+    }
+
+    if (this.cursors.left.isDown)
+    {
+        console.log('left');
+        this.player.body.velocity.x = -300;
+    }
+    else if (this.cursors.right.isDown)
+    {
+        console.log('up');
+        this.player.body.moveRight(300);
+    }
+
   }
 }
