041f555efbaa3136e4c50540852068e486ea9e73
EdwardAndress <eddie_andress@hotmail.com>
2018-12-07 00:38:57 +0000

    Player can only jump from ground

--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -6,7 +6,7 @@ export class SimpleScene extends Phaser.Scene {
       physics: {
         arcade: {
           debug: true,
-          gravity: { y: 100}
+          gravity: { y: 300}
         }
       }
     });
@@ -14,44 +14,32 @@ export class SimpleScene extends Phaser.Scene {
 
   preload() {
     this.load.image('cokecan', 'assets/cokecan.png');
+    this.load.image('ground', 'assets/ground.png')
   }
 
   create() {
     this.add.text(100, 100, 'Hello Player 1!', { fill: '#0f0' });
-    this.player = this.physics.add.sprite(100, 450, 'cokecan');
+    this.player = this.physics.add.sprite(500, 300, 'cokecan');
     this.player.setCollideWorldBounds(true);
     this.cursors = this.input.keyboard.createCursorKeys();
     var platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(2).refreshBody();
-
-    platforms.create(600, 400, 'ground');
-    platforms.create(50, 250, 'ground');
-    platforms.create(750, 220, 'ground');
+    platforms.create(400, 568, 'ground').setScale(1).refreshBody();
     this.physics.add.collider(this.player, platforms);
-    console.log(this.player)
   }
 
   update() {
 
-    if (this.cursors.up.isDown)
+    if (this.cursors.up.isDown && this.player.body.touching.down)
     {
-        console.log('up');
-        this.player.setVelocityY(-100);
-    }
-    else if (this.cursors.down.isDown)
-    {
-        console.log('down');
-        this.player.setVelocityY(100);
+        this.player.setVelocityY(-300);
     }
 
     if (this.cursors.left.isDown)
     {
-        console.log('left');
         this.player.setVelocityX(-100);
     }
     else if (this.cursors.right.isDown)
     {
-        console.log('right');
         this.player.setVelocityX(100);
     }
     else
