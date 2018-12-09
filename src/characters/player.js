4722a5baa7829d38e0063131d44c3bd7a71d8e8e
jimnarey <jimnarey@me.com>
2018-12-09 15:24:52 +0000

    Move player controls to player class

--- src/characters/player.js
+++ src/characters/player.js
@@ -1,12 +1,35 @@
 export class Player extends Phaser.GameObjects.Sprite {
   constructor(config) {
     super(config.scene, config.x, config.y, config.key);
-    config.scene.physics.world.enable(this);
+
+    //This is used in https://github.com/nkholski/phaser3-es6-webpack/blob/master/src/sprites/Mario.js
+    //but doesn't seem to be needed
+    // config.scene.physics.world.enable(this);
+
     config.scene.physics.add.existing(this);
-    console.log('Player constructor running');
+    // console.log('Player constructor running');
   }
 
   update(keys) {
+
+    if (keys.up.isDown && this.body.touching.down)
+    {
+        this.body.setVelocityY(-300);
+    }
+
+    if (keys.left.isDown)
+    {
+        this.body.setVelocityX(-100);
+    }
+    else if (keys.right.isDown)
+    {
+        this.body.setVelocityX(100);
+    }
+    else if (this.body.touching.down)
+    {
+        this.body.setVelocityX(0);
+    }
+
 
   }
 
--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -25,7 +25,7 @@ export class SimpleScene extends Phaser.Scene {
     //It seems to be a bit more flexible than createCursorKeys as you can
     //add any keyboard key in the same way
     this.keys = {
-      jump: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP),
+      up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.UP),
       left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.LEFT),
       right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.RIGHT),
       down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.DOWN)
@@ -49,24 +49,7 @@ export class SimpleScene extends Phaser.Scene {
 
   update() {
 
-    if (this.cursors.up.isDown && this.player.body.touching.down)
-    {
-        this.player.body.setVelocityY(-300);
-    }
-
-    if (this.cursors.left.isDown)
-    {
-        this.player.body.setVelocityX(-100);
-    }
-    else if (this.cursors.right.isDown)
-    {
-        this.player.body.setVelocityX(100);
-    }
-    else if (this.player.body.touching.down)
-    {
-        this.player.body.setVelocityX(0);
-    }
-
-
+    this.player.update(this.keys);
+  
   }
 }
