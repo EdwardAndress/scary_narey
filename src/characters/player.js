b88751ffa905428510505cbaf78228e57cd70231
jimnarey <jimnarey@me.com>
2018-12-09 14:03:48 +0000

    Add player class boilerplate

---
+++ src/characters/player.js
@@ -0,0 +1,12 @@
+export default class Player extends Phaser.GameObjects.Sprite {
+    constructor(config) {
+        super(config.scene, config.x, config.y, config.key);
+
+    }
+
+    update(keys, time, delta) {
+
+    }
+
+
+}
--- src/scenes/simple-scene.js
+++ src/scenes/simple-scene.js
@@ -18,34 +18,34 @@ export class SimpleScene extends Phaser.Scene {
   }
 
   create() {
-    this.add.text(100, 100, 'Hello Player 1!', { fill: '#0f0' });
-    this.player = this.physics.add.sprite(500, 300, 'cokecan');
-    this.player.setCollideWorldBounds(true);
-    this.cursors = this.input.keyboard.createCursorKeys();
-    var platforms = this.physics.add.staticGroup();
-    platforms.create(400, 568, 'ground').setScale(1).refreshBody();
-    this.physics.add.collider(this.player, platforms);
+    // this.add.text(100, 100, 'Hello Player 1!', { fill: '#0f0' });
+    // this.player = this.physics.add.sprite(500, 300, 'cokecan');
+    // this.player.setCollideWorldBounds(true);
+    // this.cursors = this.input.keyboard.createCursorKeys();
+    // var platforms = this.physics.add.staticGroup();
+    // platforms.create(400, 568, 'ground').setScale(1).refreshBody();
+    // this.physics.add.collider(this.player, platforms);
   }
 
   update() {
 
-    if (this.cursors.up.isDown && this.player.body.touching.down)
-    {
-        this.player.setVelocityY(-300);
-    }
-
-    if (this.cursors.left.isDown)
-    {
-        this.player.setVelocityX(-100);
-    }
-    else if (this.cursors.right.isDown)
-    {
-        this.player.setVelocityX(100);
-    }
-    else if (this.player.body.touching.down)
-    {
-        this.player.setVelocityX(0);
-    }
+    // if (this.cursors.up.isDown && this.player.body.touching.down)
+    // {
+    //     this.player.setVelocityY(-300);
+    // }
+    //
+    // if (this.cursors.left.isDown)
+    // {
+    //     this.player.setVelocityX(-100);
+    // }
+    // else if (this.cursors.right.isDown)
+    // {
+    //     this.player.setVelocityX(100);
+    // }
+    // else if (this.player.body.touching.down)
+    // {
+    //     this.player.setVelocityX(0);
+    // }
 
 
   }
