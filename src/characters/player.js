62263d0f8d2446be5aad97cf2fca9967c9946bfe
jimnarey <jimnarey@me.com>
2018-12-10 20:41:16 +0000

    Display coke can correctly

--- src/characters/player.js
+++ src/characters/player.js
@@ -2,32 +2,16 @@ export class Player extends Phaser.GameObjects.Sprite {
   constructor(config) {
     super(config.scene, config.x, config.y, config.key);
 
-    //This is used in https://github.com/nkholski/phaser3-es6-webpack/blob/master/src/sprites/Mario.js
-    //but doesn't seem to be needed
-    // config.scene.physics.world.enable(this);
 
-    config.scene.physics.add.existing(this);
+    config.scene.sys.displayList.add(this);
+
+    // ATM this seems to have no effect but was part of the solution I was
+    // given on Discord
+    config.scene.sys.updateList.add(this);
+
+    config.scene.sys.arcadePhysics.world.enableBody(this);
+
     this.body.setCollideWorldBounds(true);
-
-    // Following section contains attempts to get texture showing
-
-    console.log(config.key); // The string is being passed as expected
-    console.log(this.texture); // The string refers to a real item in the TextureManager
-
-    this.setActive(true); // Makes no difference
-    this.setScale(2); // Successfully doubles the size of the collider box
-    this.setVisible(true); // Makes no difference
-
-    // Doesn't throw an error, so finds texture, but no difference
-    this.setTexture('cokecan');
-
-    // Shows that we get a different result if what is passed to TextureManager
-    // isn't actually a texture within it. Note no change to the size of the
-    // collider box
-    this.setTexture('why_does_this_not_work');
-    console.log(this.texture);
-
-    // End testing section
 
   }
 
